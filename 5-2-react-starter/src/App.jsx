b99fd8942c847d3207ce070ac9fa09f5221a4108
Aljuhaymi <s202261920@ccm.kfupm.edu.sa>
2025-10-01 09:21:56 +0300

    Updating Student Web Page

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import StudentCard from './components/StudentCard'
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       <main className="dashboard-main">
         <div className="cards-container">
           {/* TODO: Import and render StudentCard components here */}
+          <div>
+            <h1>Student Info</h1>
+            <StudentCard name="Abdulmajeed Aljuhaymi" id="001" dept="22"/>
+            <StudentCard name="Rayan Alamri" id="002" dept="24"/>
+          </div>
         </div>
       </main>
     </div>
